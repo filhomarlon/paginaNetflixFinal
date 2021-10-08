652e86e8143be36f8bb99468e4d6f399e066d7dd
filhomarlon <filhomarlon@hotmail.com>
2021-10-07 23:45:28 -0300

    Pagina netflix projeto clone

---
+++ paginaNetflix/js/owl/setup.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    stagePadding: 80,
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
